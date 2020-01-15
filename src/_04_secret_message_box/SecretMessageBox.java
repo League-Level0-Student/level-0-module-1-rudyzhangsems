688836de9f06b136cff9743b86d8b9fe040f8d9a
rudyzhangsems <56939908+rudyzhangsems@users.noreply.github.com>
2020-01-15 13:49:18 -0800

    I worked on massage mox

--- src/_04_secret_message_box/SecretMessageBox.java
+++ src/_04_secret_message_box/SecretMessageBox.java
@@ -23,12 +23,16 @@ public class SecretMessageBox {
 	// 2. Using a pop-up, ask the first person for a secret message and store it in a variable
 	String message = JOptionPane.showInputDialog("what is the message");
 	// 3. Now use a pop-up to tell the NEXT user that they can only see the secret message 
-	//    if they can guess the passcode
-	String gJOptionPane.showInputDialog("What is the password");
+	//    if they can guess the password
+	String g = JOptionPane.showInputDialog("What is the password");
 	// 4. If their guess matches the password, show them the secret message
-	if(message.equals("sK%*q?RLG*=!Cfx%qHNcahUfnFVukgxW!qQvMVaLe4QL=CtMLK5$9+KJ3qwJ58c8^Yxgk%bfUXMyfE$SU*TdCgam4vQ*4%EYwF*f")) {
-		JOptionPane.showMessageDialog(null, "Access granted. " + );
+	if(password.equals("sK%*q?RLG*=!Cfx%qHNcahUfnFVukgxW!qQvMVaLe4QL=CtMLK5$9+KJ3qwJ58c8^Yxgk%bfUXMyfE$SU*TdCgam4vQ*4%EYwF*f")) {
+		JOptionPane.showMessageDialog(null, "Access granted. " + message );}
+		else {
+			JOptionPane.showMessageDialog(null, "INTRUDER ALERT. INTRUDER ALERT. INTRUDER ALERT...");
+		}
+		}
 	}
 	// 5. If the password does not match, pop-up "INTRUDER!!"
-	}
-}
+	
+
